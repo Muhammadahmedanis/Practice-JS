f95652c1c741d6134c593f5400336631a3863ad0
ahmedanis45 <ahmedanis4546@gmail.com>
2024-03-10 22:34:28 +0500

    day1-Practice

---
+++ app.js
@@ -0,0 +1,149 @@
+// GCD / HCF
+// var n1, n2, i, f;
+// n1 = +prompt("Enter N1 ");
+// n2 = +prompt("Enter N2 ");
+// for (i=1; i<n1/2 && i<n2/2; i++) 
+// {
+//     if (n1%i==0 && n2%i==0) 
+//     {
+//         f = i;
+//     }
+// }
+// alert(f);
+
+
+// var n, i, sum=0;
+// n = +prompt("Enter Number: ");
+// for (i=1; i<=n; i++) 
+// {
+//     sum += i*i;
+// }
+// alert(sum);
+
+
+// var studentName = "";
+// var myStudent = "Not enrolled";
+// if (studentName == "Ali") {
+//     myStudent = studentName;
+// }else if (studentName == "Zain"){
+//     myStudent = studentName;
+// }
+// alert(myStudent);
+
+
+// var num1 = 3;
+// var num2 = -5;
+// var num3 = 2;
+// var result = num1*num2*num3;
+// var output = "invalid input";
+// if (result > 0) {
+//     output = result + " positive number";
+// }else {
+//     output = result + " Negative number";
+// } 
+// alert(output)
+
+
+// var a = 2;
+// var b = 0;
+// var c = -4;
+// var d = 7;
+// var e = 9;
+// var output = "Invalid";
+
+// if (a > b && a > c && a > d && a > e) {
+//     output = `${a} is greater`;
+// } else if (b > a && b > c && b > d && b > e) {
+//     output = `${b} is greater`;
+// }
+// else if (c > a && c > b && c > d && c > e) {
+//     output = `${c} is greater`;
+// }
+// else if (d > a && d > b && d > c && d > e) {
+//     output = `${d} is greater`;
+// }
+// else {
+//     output = `${e} is greater`;
+// }
+// alert(output);
+
+
+
+// var arr = ["Apple", "Mango", "Banana", "Orange"];
+// arr.push("Peach");
+// arr.pop()
+// arr.splice(2,1);
+// var arr2 = arr.slice(0,2)
+// document.write(arr, "<br/>",arr2)
+
+// for(var i=0; i<arr.length; i++) {
+//     document.write(arr[i] + "<br/>");
+// }
+
+
+
+// var arr = ["Bmw", "Audi", "Cultus"];
+// var input = prompt("Enter value");
+// var cap = input.slice(0,1).toUpperCase();
+// var sm = input.slice(1).toLowerCase();
+// var total = cap + sm;
+// var msg =  "not available"; 
+// for (var i=0; i<arr.length; i++) {   
+//     if (arr[i] == total) {
+//         msg = total;
+//         break; 
+//     }
+// }
+// alert(msg);
+
+
+// arr[1] = "Vigo"
+// document.write(arr)
+
+
+
+
+
+
+
+
+
+
+
+
+// check
+// var input = +prompt("Enter number: ");
+// var p, i;
+// while (input > 0) {
+//     i = input % 10;
+//     p += i ** 3;
+//     input /= 10;
+// }
+// document.write(p);
+
+
+
+
+
+
+
+
+
+// var a = 2;
+// a **= 3;
+//  document.write(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
